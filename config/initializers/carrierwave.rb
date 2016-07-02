57b377bab3f9cb41d318685c0ce4fb20f95da069
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2016-07-02 15:47:25 +0000

    Fixed missing commas

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS'
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"]
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],
     :aws_secrete_access_key => ENV["AWS_SECRETE_KEY"]
   }
   config.fog_directory = ENV["AWS_BUCKET"]
