56d4e9841abf0b4cf5cca4f3520b230429709a4b
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2016-07-03 11:23:56 +0000

    Added permission to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course, only: [:show]
+
   def new
   end
 
   private
+
+  def require_enrollment_for_current_course
+    current_course = current_lesson.section.course
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_url(current_lesson.section.course), alert: 'Must Enroll to View Lessons!'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
